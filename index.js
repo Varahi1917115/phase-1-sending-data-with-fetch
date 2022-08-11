ebeceaa0b0110e25436f995d6174f46517866917
Samarth <Varahi1917115@gmail.com>
2022-08-10 23:34:54 -0400

    commit tree c08a80270e81787c0e120e9c8902b13ff8bc4825

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const { overrideRequests } = require("nock/lib/common");
+
+const body = document.getElementsByTagName("body")[0];
+function submitData(firstName, email) {
+    const confgObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": firstName,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", confgObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(response => 'Unauthorized Access')
+}
+function addNewContact(newID){
+    document.getElementsByTagName("body")[0].innerHTML = newID;
+    document.body.append(message)
+}
+submitData("Name", "name@name.com");
+
